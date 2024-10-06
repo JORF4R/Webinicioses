89330dbd4af42e9dcccf942833ee3fe499ba6c9b
JORF4R <123892522+JORF4R@users.noreply.github.com>
2024-10-06 16:22:00 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event){
     const password = document.getElementById("password").value;
 
     if(username === "admin" && password === "admin123") {
-        window.location.href = "index.html";
+        window.location.href = "tabla.html";
     } else {
         alert("Credenciales incorrectas, intente nuevamente.");
     }
